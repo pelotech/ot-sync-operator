ba3b44e40514753911df7b7aec4a0a1f6450b8e4
Harrison Billings <harrison.billings@pelo.tech>
2025-07-23 15:58:17 -0700

    chore: removed unneeded files after refactor pass

--- internal/resource-deployer/types.go
+++
@@ -1,8 +0,0 @@
-package resourcedeployer
-
-import "context"
-
-type ResourceDeployer[T any] interface {
-	Deploy(ctx context.Context, item *T) (*T, error)
-	Destroy(ctx context.Context, item *T) (*T, error)
-}
