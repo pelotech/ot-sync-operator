798d04ff68576bc385b79cd59ee4ed1cb962213a
Harrison Billings <harrison.billings@pelo.tech>
2025-07-22 21:55:51 -0700

    refactor: go mods now happy resourcegenerator is in

--- go.mod
+++ go.mod
@@ -46,6 +46,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/openshift/custom-resource-status v1.1.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/prometheus/client_golang v1.22.0 // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
@@ -90,6 +91,8 @@ require (
 	k8s.io/klog/v2 v2.130.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20250318190949-c8a335a9a2ff // indirect
 	k8s.io/utils v0.0.0-20241104100929-3ea5e8cea738 // indirect
+	kubevirt.io/containerized-data-importer-api v1.62.0 // indirect
+	kubevirt.io/controller-lifecycle-operator-sdk/api v0.0.0-20220329064328-f3cc58c6ed90 // indirect
 	sigs.k8s.io/apiserver-network-proxy/konnectivity-client v0.31.2 // indirect
 	sigs.k8s.io/json v0.0.0-20241010143419-9aa6b5e7a4b3 // indirect
 	sigs.k8s.io/randfill v1.0.0 // indirect
--- internal/resource-generator/resource-generator.go
+++ internal/resource-generator/resource-generator.go
@@ -1,17 +1,19 @@
 package resourcegenerator
 
 import (
-	// "errors"
-	// "fmt"
-	// "math"
-	// "regexp"
-	// "strconv"
+	"errors"
+	"fmt"
+	"math"
+	"regexp"
+	"strconv"
 
 	crdv1 "pelotech/ot-sync-operator/api/v1"
 
-	// corev1 "k8s.io/api/core/v1"
-	// "k8s.io/apimachinery/pkg/api/resource"
 	snapshotv1 "github.com/kubernetes-csi/external-snapshotter/client/v6/apis/volumesnapshot/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	cdiv1beta1 "kubevirt.io/containerized-data-importer-api/pkg/apis/core/v1beta1"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 )
@@ -29,81 +31,82 @@ type DataVolumeConfig struct {
 	StorageClass *string
 }
 
-// func CreateDataVolume(config *DataVolumeConfig) (*cdi.DataVolume, error) {
-// 	diskSize, err := calculateDiskSize(config.Vm.Name, config.AddDiskSpace)
+func CreateDataVolume(config *DataVolumeConfig) (*cdiv1beta1.DataVolume, error) {
+	diskSize, err := calculateDiskSize(config.Vm.Name, config.AddDiskSpace)
 
-// 	if err != nil {
-// 		return nil, err
-// 	}
+	if err != nil {
+		return nil, err
+	}
 
-// 	blockOwnerDeletion := true
-// 	ownerReferences := []metav1.OwnerReference{
-// 		{
-// 			APIVersion:         "v1",
-// 			BlockOwnerDeletion: &blockOwnerDeletion,
-// 			Kind:               "DataSync",
-// 			Name:               config.OwnerName,
-// 			UID:                config.OwnerUID,
-// 		},
-// 	}
+	blockOwnerDeletion := true
+	ownerReferences := []metav1.OwnerReference{
+		{
+			APIVersion:         "v1",
+			BlockOwnerDeletion: &blockOwnerDeletion,
+			Kind:               "DataSync",
+			Name:               config.OwnerName,
+			UID:                config.OwnerUID,
+		},
+	}
 
-// 	meta := metav1.ObjectMeta{
-// 		Name:            config.ResourceName,
-// 		Namespace:       config.Namespace,
-// 		Labels:          config.Labels,
-// 		OwnerReferences: ownerReferences,
-// 		Annotations: map[string]string{
-// 			"cdi.kubevirt.io/storage.bind.immediate.requested": "true",
-// 		},
-// 	}
+	meta := metav1.ObjectMeta{
+		Name:            config.ResourceName,
+		Namespace:       config.Namespace,
+		Labels:          config.Labels,
+		OwnerReferences: ownerReferences,
+		Annotations: map[string]string{
+			"cdi.kubevirt.io/storage.bind.immediate.requested": "true",
+		},
+	}
 
-// 	pvc := &corev1.PersistentVolumeClaimSpec{
-// 		AccessModes: []corev1.PersistentVolumeAccessMode{corev1.ReadWriteOnce},
-// 		Resources: corev1.VolumeResourceRequirements{
-// 			Requests: corev1.ResourceList{
-// 				corev1.ResourceStorage: resource.MustParse(diskSize),
-// 			},
-// 		},
-// 	}
+	pvc := &corev1.PersistentVolumeClaimSpec{
+		AccessModes: []corev1.PersistentVolumeAccessMode{corev1.ReadWriteOnce},
+		Resources: corev1.VolumeResourceRequirements{
+			Requests: corev1.ResourceList{
+				corev1.ResourceStorage: resource.MustParse(diskSize),
+			},
+		},
+	}
 
-// 	if config.StorageClass != nil {
-// 		pvc.StorageClassName = config.StorageClass
-// 	}
+	if config.StorageClass != nil {
+		pvc.StorageClassName = config.StorageClass
+	}
 
-// 	var source *cdi.DataVolumeSource
+	var source *cdiv1beta1.DataVolumeSource
 
-// 	if config.Vm.SourceType == "s3" {
-// 		source = &cdi.DataVolumeSource{
-// 			S3: &cdi.DataVolumeSourceS3{
-// 				URL:       config.Vm.URL,
-// 				SecretRef: config.SecretRef,
-// 			},
-// 		}
-// 	} else {
-// 		if config.ConfigMapRef == nil {
-// 			return nil, errors.New("Attempted to create a datavolume without a registry but no certConfigMap was provided.")
-// 		}
-// 		source = &cdi.DataVolumeSource{
-// 			Registry: &cdi.DataVolumeSourceRegistry{
-// 				URL:           &config.Vm.URL,
-// 				CertConfigMap: config.ConfigMapRef,
-// 				SecretRef:     &config.SecretRef,
-// 			},
-// 		}
-// 	}
+	if config.Vm.SourceType == "s3" {
+		source = &cdiv1beta1.DataVolumeSource{
+			S3: &cdiv1beta1.DataVolumeSourceS3{
+				URL:       config.Vm.URL,
+				SecretRef: config.SecretRef,
+			},
+		}
+	} else {
+		if config.ConfigMapRef == nil {
+			errMsg := "attempted to create a datavolume without a registry but no certConfigMap was provided"
+			return nil, errors.New(errMsg)
+		}
+		source = &cdiv1beta1.DataVolumeSource{
+			Registry: &cdiv1beta1.DataVolumeSourceRegistry{
+				URL:           &config.Vm.URL,
+				CertConfigMap: config.ConfigMapRef,
+				SecretRef:     &config.SecretRef,
+			},
+		}
+	}
 
-// 	spec := cdi.DataVolumeSpec{
-// 		PVC:    pvc,
-// 		Source: source,
-// 	}
+	spec := cdiv1beta1.DataVolumeSpec{
+		PVC:    pvc,
+		Source: source,
+	}
 
-// 	dv := &cdi.DataVolume{
-// 		ObjectMeta: meta,
-// 		Spec:       spec,
-// 	}
+	dv := &cdiv1beta1.DataVolume{
+		ObjectMeta: meta,
+		Spec:       spec,
+	}
 
-// 	return dv, nil
-// }
+	return dv, nil
+}
 
 type VolumeSnapshotConfig struct {
 	ResourceName string
@@ -137,25 +140,25 @@ func CreateVolumeSnapshot(config VolumeSnapshotConfig) *snapshotv1.VolumeSnapsho
 	}
 }
 
-// func calculateDiskSize(diskSize string, addDiskSize bool) (string, error) {
-// 	re := regexp.MustCompile(`([0-9.]+)([a-zA-Z]+)`)
+func calculateDiskSize(diskSize string, addDiskSize bool) (string, error) {
+	re := regexp.MustCompile(`([0-9.]+)([a-zA-Z]+)`)
 
-// 	matches := re.FindStringSubmatch(diskSize)
+	matches := re.FindStringSubmatch(diskSize)
 
-// 	if len(matches) != 3 {
-// 		return "", fmt.Errorf("invalid disk size format: %s", diskSize)
-// 	}
+	if len(matches) != 3 {
+		return "", fmt.Errorf("invalid disk size format: %s", diskSize)
+	}
 
-// 	if !addDiskSize {
-// 		return diskSize, nil
-// 	}
+	if !addDiskSize {
+		return diskSize, nil
+	}
 
-// 	numPart, err := strconv.ParseFloat(matches[1], 64)
-// 	if err != nil {
-// 		return "", err
-// 	}
-// 	unitPart := matches[2]
+	numPart, err := strconv.ParseFloat(matches[1], 64)
+	if err != nil {
+		return "", err
+	}
+	unitPart := matches[2]
 
-// 	augmentedNum := math.Ceil(numPart * 1.33)
-// 	return fmt.Sprintf("%d%s", int(augmentedNum), unitPart), nil
-// }
+	augmentedNum := math.Ceil(numPart * 1.33)
+	return fmt.Sprintf("%d%s", int(augmentedNum), unitPart), nil
+}
