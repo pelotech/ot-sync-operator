c1caeda248ec1d588fbab2c66bdcebbb677af615
Harrison Billings <harrison.billings@pelo.tech>
2025-07-23 15:58:00 -0700

    refactor: now using our resource manager in controller to create and remove stuff plus added rbac

--- internal/controller/datasync_controller.go
+++ internal/controller/datasync_controller.go
@@ -16,19 +16,29 @@ import (
 	crdv1 "pelotech/ot-sync-operator/api/v1"
 	controllerutil "pelotech/ot-sync-operator/internal/contoller-utils"
 	generalutils "pelotech/ot-sync-operator/internal/general-utils"
+	resourcemanager "pelotech/ot-sync-operator/internal/resource-manager"
 )
 
 // DataSyncReconciler reconciles a DataSync object
 type DataSyncReconciler struct {
 	client.Client
-	Scheme   *runtime.Scheme
-	Recorder record.EventRecorder
+	Scheme          *runtime.Scheme
+	Recorder        record.EventRecorder
+	ResourceManager resourcemanager.ResourceManager[crdv1.DataSync]
 }
 
+// RBAC for our CRD
 // +kubebuilder:rbac:groups=crd.pelotech.ot,resources=datasyncs,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=crd.pelotech.ot,resources=datasyncs/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=crd.pelotech.ot,resources=datasyncs/finalizers,verbs=update
+
+// RBAC so we can watch configmaps in the cluster from the controller
 // +kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list;watch
+
+// RBAC preform CRUD operations on pvcs, datavolumes and volumesnapshots
+// +kubebuilder:rbac:groups="",resources=persistentvolumeclaims,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=cdi.kubevirt.io,resources=datavolumes,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=snapshot.storage.k8s.io,resources=volumesnapshots,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -43,7 +53,6 @@ const defaultRetryLimit = 2
 const defaultBackoffLimit = 120 * time.Second
 
 func (r *DataSyncReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-
 	logger := logf.FromContext(ctx)
 
 	var dataSync crdv1.DataSync
@@ -56,10 +65,7 @@ func (r *DataSyncReconciler) Reconcile(ctx context.Context, req ctrl.Request) (c
 		return ctrl.Result{}, err
 	}
 
-	// 2. Get the configmap values for operator to abide by
-	// TODO: Should we have more robust error handling if the configmap isn't there?
 	configMapName := "datasync-operator-config"
-
 	operatorConfigmap, err := generalutils.GetConfigMap(ctx, r.Client, configMapName, dataSync.Namespace)
 	if err != nil {
 		logger.Error(err, "Failed to get operator config")
@@ -129,6 +135,13 @@ func (r *DataSyncReconciler) attemptSyncingOfResource(ctx context.Context, ds *c
 	if len(syncingList.Items) >= syncLimit {
 		r.Recorder.Eventf(ds, "Normal", "WaitingToSync", "No more than %d DataSyncs can be syncing at once. Waiting...", syncLimit)
 		return ctrl.Result{RequeueAfter: requeueTimeInveral}, nil
+	}
+
+	err = r.ResourceManager.CreateResources(ctx, r.Client, ds)
+
+	if err != nil {
+		r.Recorder.Eventf(ds, "Warning", "ResourceCreationFailed", "Failed to create resources.")
+		return r.handleResourceCreationError(ctx, ds, err)
 	}
 
 	ds.Status.Phase = crdv1.DataSyncPhaseSyncing
@@ -207,9 +220,35 @@ func (r *DataSyncReconciler) handleResourceUpdateError(ctx context.Context, ds *
 	return ctrl.Result{}, originalErr
 }
 
-// TODO: This is where we want to do error handling for sync specific errors
-//
-//	We need to implement retry. These values are found in the configmap
+func (r *DataSyncReconciler) handleResourceCreationError(ctx context.Context, ds *crdv1.DataSync, originalErr error) (ctrl.Result, error) {
+	logger := logf.FromContext(ctx)
+	logger.Error(originalErr, "Failed to create a resource when trying to intiate resource sync")
+
+	r.Recorder.Eventf(ds, "Warning", "ResourceCreationFailed", "Failed to create resources.")
+
+	ds.Status.Phase = crdv1.DataSyncPhaseFailed
+	ds.Status.Message = "Failed while creating resources: " + originalErr.Error()
+	meta.SetStatusCondition(&ds.Status.Conditions, metav1.Condition{
+		Type:    crdv1.DataSyncTypeReady,
+		Status:  metav1.ConditionFalse,
+		Reason:  "ResourceCreationFailed",
+		Message: originalErr.Error(),
+	})
+
+	if err := r.Status().Update(ctx, ds); err != nil {
+		logger.Error(err, "Could not update status to Failed resource creation failure")
+	}
+
+	err := r.ResourceManager.TearDownAllResources(ctx, r.Client, ds)
+
+	if err != nil {
+		logger.Error(err, "Failed to teardown resources.")
+	}
+
+	return ctrl.Result{}, originalErr
+
+}
+
 func (r *DataSyncReconciler) handleSyncError(ctx context.Context, ds *crdv1.DataSync, originalErr error, message string, retryLimit int, retryBackoff time.Duration) (ctrl.Result, error) {
 	logger := logf.FromContext(ctx)
 	logger.Error(originalErr, message)
@@ -242,6 +281,12 @@ func (r *DataSyncReconciler) handleSyncError(ctx context.Context, ds *crdv1.Data
 	// Attempt to update the status to Failed, but return the original error
 	if err := r.Status().Update(ctx, ds); err != nil {
 		logger.Error(err, "Could not update status to Failed after an initial update error")
+	}
+
+	err := r.ResourceManager.TearDownAllResources(ctx, r.Client, ds)
+
+	if err != nil {
+		logger.Error(err, "Failed to teardown resources.")
 	}
 
 	return ctrl.Result{}, originalErr
